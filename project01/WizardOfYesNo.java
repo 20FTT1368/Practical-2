6da2fba579e8338b74078f2f5b7a97cbc8e512de
Fiqahhassan <fiqah.hjhassan@gmail.com>
2022-02-11 19:51:35 +0800

    Lesson 1.5 - Label Text using Variable

--- project01/WizardOfYesNo.java
+++ project01/WizardOfYesNo.java
@@ -7,22 +7,32 @@ import javafx.scene.Scene;
 
 
 public class WizardOfYesNo extends Application {
-
+	private static final String ANSWER = "No";
 	@Override
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
-		Label label = new Label("Hello World");
-		 
+	//	Label label = new Label("Yes");
+		 		   Label label = new Label();
+	 label.setText(ANSWER);
 		// Add the two arguments for width and height. In this case it is 200 width x 100 height.
 		  Scene root = new Scene(label, 200, 100);
 		  arg0.setScene(root);
+
+		  
 		 
 		arg0.show();
 	}
+	
+
+	
+
+	
 
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
+	
+	
 	
 	
 }
