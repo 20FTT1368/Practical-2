6f10a3fbc28d47972afb346aa56716a894d27054
Fiqahhassan <fiqah.hjhassan@gmail.com>
2022-02-11 18:57:10 +0800

    Lesson 1.3 - Creating a Window

---
+++ project01/WizardOfYesNo.java
@@ -0,0 +1,28 @@
+package project01;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+
+
+public class WizardOfYesNo extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		Label label = new Label("Hello World");
+		 
+		// Add the two arguments for width and height. In this case it is 200 width x 100 height.
+		  Scene root = new Scene(label, 200, 100);
+		  arg0.setScene(root);
+		 
+		arg0.show();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
+	
+}
